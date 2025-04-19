35931e317f781f5e7df994367fe614e26f66282e
robbiewinter <robbie.winter@myy.haaga-helia.fi>
2025-04-19 17:29:14 +0300

    Add @Bean
    Add 2 loans to loan-list. One with compound interest, and one with simple interest

--- src/main/java/fi/robbie/loan/loan_calculator/LoanCalculatorApplication.java
+++ src/main/java/fi/robbie/loan/loan_calculator/LoanCalculatorApplication.java
@@ -1,7 +1,14 @@
 package fi.robbie.loan.loan_calculator;
 
+import fi.robbie.loan.loan_calculator.domain.LoanInfo;
+import fi.robbie.loan.loan_calculator.domain.LoanInfoRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class LoanCalculatorApplication {
@@ -10,4 +17,30 @@ public class LoanCalculatorApplication {
 		SpringApplication.run(LoanCalculatorApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initializeLoans(LoanInfoRepository loanInfoRepository) {
+		return args -> {
+			// Create a loan with simple interest
+			LoanInfo simpleLoan = new LoanInfo();
+			simpleLoan.setLoanAmount(10000);
+			simpleLoan.setInterestRate(10);
+			simpleLoan.setLoanTerm(12);
+			simpleLoan.setCalculationType("SIMPLE");
+			simpleLoan.setLoanName("Simple Interest");
+			simpleLoan.setStartDate(LocalDate.now());
+
+			// Create a loan with compound interest
+			LoanInfo compoundLoan = new LoanInfo();
+			compoundLoan.setLoanAmount(10000);
+			compoundLoan.setInterestRate(10);
+			compoundLoan.setLoanTerm(12);
+			compoundLoan.setCalculationType("COMPOUND");
+			compoundLoan.setLoanName("Compound Interest");
+			compoundLoan.setStartDate(LocalDate.now());
+
+			// Save both loans to the repository
+			loanInfoRepository.save(simpleLoan);
+			loanInfoRepository.save(compoundLoan);
+		};
+	}
 }
